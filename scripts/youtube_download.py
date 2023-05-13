de5cde21744477c16867499780870946d9a908e7
Brandon Hancock <brandon@brandonhancock.io>
2023-05-13 08:19:28 -0400

    Code in working state.

--- scripts/youtube_download.py
+++ scripts/youtube_download.py
@@ -1,11 +1,7 @@
-"""
-TODO:
-- need to pass in the video id instead of the index on line 37.
-
-"""
-
+import argparse
 import csv
-import yt_dlp  # replace youtube_dl with yt_dlp
+import yt_dlp
+from urllib.parse import parse_qs, urlparse
 
 
 def download_audio(url, output_path):
@@ -13,26 +9,45 @@ def download_audio(url, output_path):
         'format': 'bestaudio/best',
         'outtmpl': output_path,
         'postprocessors': [{
-            'key': 'FFmpegExtractAudio',  # this remains the same
+            'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        'SponsorBlock': False,  # this is a yt-dlp option, it's not a postprocessor
+        'SponsorBlock': False,
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
 
-# Read YouTube URLs from a CSV file
-with open('./scripts/youtube_urls.csv', 'r') as f:
-    youtube_urls = f.read().splitlines()
+def extract_video_id(url):
+    parsed_url = urlparse(url)
+    video_id = parse_qs(parsed_url.query).get('v', [None])[0]
+    return video_id
 
-for i, url in enumerate(youtube_urls):
-    url = url.strip()  # Remove leading/trailing whitespace
-    if not url or not url.startswith("http"):
-        # Skip if the URL is empty or doesn't start with http
-        print(f"Skipping invalid URL: '{url}'")
-        continue
-    # This will save the files as audio0.mp3, audio1.mp3, etc.
-    output_path = f'./audio_files/audio{i}.mp3'
-    download_audio(url, output_path)
+
+def main(csv_file):
+    with open(csv_file, 'r') as f:
+        youtube_urls = f.read().splitlines()
+
+    for url in youtube_urls:
+        url = url.strip()
+        if not url or not url.startswith("http"):
+            print(f"Skipping invalid URL: '{url}'")
+            continue
+        video_id = extract_video_id(url)
+        print("video_id", video_id)
+        if video_id is None:
+            print(f"Couldn't extract video ID from URL: '{url}'")
+            continue
+        output_path = f'./audio_files/{video_id}'
+        download_audio(url, output_path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Download audio from YouTube URLs in a CSV file')
+    parser.add_argument('csv_file', type=str,
+                        help='Path to the CSV file containing YouTube URLs')
+    args = parser.parse_args()
+
+    main(args.csv_file)
